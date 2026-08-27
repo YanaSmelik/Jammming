8cdc4d5b012407f304c4d2d0f8a374f20d97c49b
Yana <smelikyana@gmail.com>
2026-08-27 15:44:40 +0200

    Add props into Track

--- Jammming/src/components/Track.jsx
+++ Jammming/src/components/Track.jsx
@@ -1,10 +1,13 @@
 import styles from '../styles/track.module.css';
 
-function Track(){
+function Track(props){
 
      return (
       <div className={styles.container}>
     <h1>Track</h1>
+    <p>{props.name}</p>
+    <p>{props.artist}</p>
+    <p>{props.album}</p>
     </div>
  );
 }
