7912358d21f43c3aa0e0fe6f3c4f6fce5a35a71e
yana smelyk <smelikyana@gmail.com>
2026-08-30 14:16:50 +0200

    Add handleSearch functionality

--- Jammming/src/App.jsx
+++ Jammming/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import './App.css'
-import Playlist from './components/Playlist';
-import SearchBar from './components/SearchBar';
-import SearchResults from './components/SearchResults';
-import Track from './components/Track';
-import Tracklist from './components/Tracklist';
-import styles from './styles/app.module.css';
-import tracks from './data/tracks.json';
-
+import { useState } from "react";
+import "./App.css";
+import Playlist from "./components/Playlist";
+import SearchBar from "./components/SearchBar";
+import SearchResults from "./components/SearchResults";
+import Track from "./components/Track";
+import Tracklist from "./components/Tracklist";
+import styles from "./styles/app.module.css";
+import tracks from "./data/tracks.json";
 
 function App() {
-  const[playlist, setPlaylist] = useState([]);
-  const[searchResults, setSearchResults] = useState(tracks);
+  const [playlist, setPlaylist] = useState([]);
+  const [searchResults, setSearchResults] = useState(tracks);
+
+  const handleSearch = (searchInput) => {
+    getData();
+  };
+
+  //TODO CORS proxi, query for url
+  async function getData() {
+    const url = "https://api.deezer.com/user/2529/playlistshttps://musicbrainz.org/ws/2/recording?fmt=json&query=%22we%20will%20rock%20you%22%20AND%20arid:0383dadf-2a4e-4d10-a46a-e9e041da8eb3";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
   return (
     <>
       <div className={styles.container}>
         <div>
-          <SearchBar />
+          <SearchBar handleSearch={handleSearch} />
         </div>
         <div className={styles.mainContent}>
-
           <div>
-            <SearchResults tracklist={searchResults}/>
+            <SearchResults tracklist={searchResults} />
             <Playlist tracklist={searchResults} />
           </div>
         </div>
         <Track />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Jammming/src/components/SearchBar.jsx
+++ Jammming/src/components/SearchBar.jsx
@@ -1,11 +1,28 @@
-import styles from '../styles/searchBar.module.css';
+import styles from "../styles/searchBar.module.css";
+import { useState } from 'react'
 
-function SearchBar(){
- return (
-   <div className={styles.container}>
-    <h1>Search Bar</h1>
+function SearchBar(props) {
+  const [search, setSearch] = useState("");
+
+  return (
+    <div className={styles.container}>
+      <h1>Search</h1>
+      <input
+        type="text"
+        placeholder="What are you looking for?"
+        onChange={(event) => {
+          setSearch(event.target.value);
+        }}
+      ></input>
+      <button
+        onClick={(event) => {
+          props.handleSearch(search);
+        }}
+      >
+        Search
+      </button>
     </div>
- );
+  );
 }
 
 export default SearchBar;
